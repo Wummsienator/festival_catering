40d7bd541bc8b4dbb53bbb6fd757c6d6633002db
Wummsienator <wummsienator@gmail.com>
2025-09-02 23:01:18 +0200

    Much new stuff

--- database.py
+++ database.py
@@ -2,20 +2,94 @@ import json
 
 class Database:
     def __init__(self):
-        self.data = None
-        self.readData()
         return
     
     def readData(self):
         # Read from file and parse JSON
         with open("data.json", "r") as f:
             data = json.load(f)
-        self.data = data
+        return data
+    
+    def writeData(self, data):
+        json_str = json.dumps(data, indent=4)
+        with open("data.json", "w") as f:
+            f.write(json_str)
+    
+    def getproductsForStand(self, stand):
+        products = []
+        data = self.readData()
+        for p in data["Stands"][stand]["products"]:
+            products.append(data["Products"][p])
+        return products
+    
+    def adjustTimers(self):
+        data = self.readData()
+        #adjust timers
+        for o in data["Orders"]:
+            if data["Orders"][o]["time"] > 0:
+                data["Orders"][o]["time"] = data["Orders"][o]["time"] - 1
+        self.writeData(data)
 
-    def getData(self):
-        return self.data
+    def getOrdersForTicket(self, ticket):
+        orders = []
+        data = self.readData()
+        for o2t in data["Order2Ticket"]:
+            #get relation object
+            o2tObj = data["Order2Ticket"][o2t]
+            if o2tObj["ticket"] == ticket:
+                order = data["Orders"][o2tObj["order"]]
+                #append stand number
+                for o2s in data["Order2Stand"]:
+                    o2sObj = data["Order2Stand"][o2s]
+                    if o2sObj["order"] == o2tObj["order"]:
+                        order["stand"] = o2sObj["stand"]
+                #get status description
+                for s in data["Status"]:
+                    if s == order["status"]:
+                        order["status_desc"] = data["Status"][s]
+                orders.append(order)
+        return orders
+    
+    def placeOrder(self, stand, ticket, positionList):
+        data = self.readData()
+        time = 0
+        price = 0
+        positions = {}
+
+        #add positions
+        counter = 1
+        for p in positionList:
+            prod = data["Products"][p["product"]]
+            price += p["quantity"] * prod["price"]
+            time += p["quantity"] * prod["time"]
+            positions[str(counter)] = p
+            counter += 1
+
+        #add order
+        data["Orders"][data["GlobalIDs"]["Orders"]] = {"time": time, "price": price, "status": "1", "positions": positions}
+
+        #add order2stand
+        data["Order2Stand"][data["GlobalIDs"]["Order2Stand"]] = {"order": str(data["GlobalIDs"]["Orders"]), "stand": stand}
+
+        #add order2ticket
+        data["Order2Ticket"][data["GlobalIDs"]["Order2Ticket"]] = {"order": str(data["GlobalIDs"]["Orders"]), "ticket": ticket}
+
+        #update ids
+        data["GlobalIDs"]["Orders"] = data["GlobalIDs"]["Orders"] + 1
+        data["GlobalIDs"]["Order2Stand"] = data["GlobalIDs"]["Order2Stand"] + 1
+        data["GlobalIDs"]["Order2Ticket"] = data["GlobalIDs"]["Order2Ticket"] + 1
+
+        self.writeData(data)
+        
 
 database = Database()
-data = database.getData()
 
-print(data)
+# print(database.getproductsForStand("1"))
+# print(database.getproductsForStand("3"))
+
+print(database.getOrdersForTicket("1234567"))
+print(database.getOrdersForTicket("8910111"))
+
+#database.adjustTimers()
+
+# database.placeOrder("2", "8910111", [{"product": "3", "quantity": 1}])
