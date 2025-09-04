2cbc3e7aa0db5f1dc80543983aa728a29fe32679
hendrik.wolf <hendrik.wolf@ilcgroup.com>
2025-09-04 09:11:59 +0200

    test

--- besucher.py
+++ besucher.py
@@ -4,147 +4,81 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from database import Database
 
-# root = Tk()
-
-
-# #styles
-# style1 = font.Font(size=20)
-
-# #variables
-# database = Database()
-
-# #logo
-# logo_pil = Image.open("logo.png")
-# logo_pil = logo_pil.resize((50, 50), Image.Resampling.LANCZOS)
-
-# logo_img = ImageTk.PhotoImage(logo_pil)
-
-# # Title label
-# title = Label(
-#     root,
-#     text="Offene Bestellungen:",
-#     font=("Arial", 14, "bold"),
-#     bg="#05445E",   # dark teal background
-#     fg="white"
-# )
-# title.pack(fill="x")
-
-# # Define table columns
-# columns = ("Stand", "Wartezeit", "Status", "Bestellungsnummer")
-# table = ttk.Treeview(root, columns=columns, show="headings", selectmode="browse")
-
-# # Define headings
-# for col in columns:
-#     table.heading(col, text=col)
-#     table.column(col, anchor="center", width=140)
-
-# # Insert sample data
-# data = []
-# orders = database.getOrdersForTicket("1234567")
-# for order in orders:
-#     data.append( (order["stand"], order["time"], order["status_desc"], order["order"]) )
-
-# for i, row in enumerate(data):
-#     # tag = "evenrow" if i % 2 == 0 else "oddrow"
-#     # table.insert("", END, values=row, tags=(tag,))
-#     table.insert("", END, values=row, tags=("evenrow",))
-
-# # Style rows
-# style = ttk.Style(root)
-# style.theme_use("default")
-
-# # Header style
-# style.configure("Treeview.Heading", font=("Arial", 11, "bold"), background="#05445E", foreground="white")
-
-# # Row styles
-# style.configure("Treeview", font=("Arial", 11), rowheight=25)
-# table.tag_configure("evenrow", background="#D4F1F4")   # baby blue
-# # table.tag_configure("oddrow", background="white")
-
-# # Pack table
-# table.pack(expand=True, fill="both", padx=5, pady=5)
-
-# # Selection highlight
-# def on_select(event):
-#     selected = table.focus()
-#     values = table.item(selected, "values")
-#     print("Selected row:", values)
-
-# table.bind("<<TreeviewSelect>>", on_select)
-
-# #settings
-# root.geometry("950x950")
-# root.title("Festival Catering")
-# root.resizable(False, False)
-# root.mainloop()
-
 class BesucherPage():
     def __init__(self, root, database, style1):
         self._root = root
         self._database = database
         self._style1 = style1
+        self._besucherPage = ""
 
     def getPage(self):
-        #page
-        besucherPage = Frame(self._root)
+        if not self._besucherPage:
+            #page
+            besucherPage = Frame(self._root)
 
+            self.loadLogoImg()
+            self.createOrderTable(besucherPage)
+
+            self._besucherPage  = besucherPage
+        return self._besucherPage
+    
+    def loadLogoImg(self):
         #logo
         logo_pil = Image.open("logo.png")
         logo_pil = logo_pil.resize((50, 50), Image.Resampling.LANCZOS)
 
-        logo_img = ImageTk.PhotoImage(logo_pil)
+        self._logo_img = ImageTk.PhotoImage(logo_pil)
 
+    
+    def createOrderTable(self, besucherPage):
         # Title label
-        title = Label(
-            besucherPage,
-            text="Offene Bestellungen:",
-            font=("Arial", 14, "bold"),
-            bg="#05445E",   # dark teal background
-            fg="white"
-        )
-        title.pack(fill="x")
+            title = Label(
+                besucherPage,
+                text="Offene Bestellungen:",
+                font=("Arial", 14, "bold"),
+                bg="#05445E",   # dark teal background
+                fg="white"
+            )
+            title.pack(fill="x")
 
-        # Define table columns
-        columns = ("Stand", "Wartezeit", "Status", "Bestellungsnummer")
-        table = ttk.Treeview(besucherPage, columns=columns, show="headings", selectmode="browse")
+            # Define table columns
+            columns = ("Stand", "Wartezeit", "Status", "Bestellungsnummer")
+            self.table = ttk.Treeview(besucherPage, columns=columns, show="headings", selectmode="browse")
 
-        # Define headings
-        for col in columns:
-            table.heading(col, text=col)
-            table.column(col, anchor="center", width=140)
+            # Define headings
+            for col in columns:
+                self.table.heading(col, text=col)
+                self.table.column(col, anchor="center", width=140)
 
-        # Insert sample data
-        data = []
-        orders = self._database.getOrdersForTicket("1234567")
-        for order in orders:
-            data.append( (order["stand"], order["time"], order["status_desc"], order["order"]) )
+            # Insert sample data
+            data = []
+            orders = self._database.getOrdersForTicket("1234567")
+            for order in orders:
+                data.append( (order["stand"], order["time"], order["status_desc"], order["order"]) )
 
-        for i, row in enumerate(data):
-            # tag = "evenrow" if i % 2 == 0 else "oddrow"
-            # table.insert("", END, values=row, tags=(tag,))
-            table.insert("", END, values=row, tags=("evenrow",))
+            for i, row in enumerate(data):
+                # tag = "evenrow" if i % 2 == 0 else "oddrow"
+                # table.insert("", END, values=row, tags=(tag,))
+                self.table.insert("", END, values=row, tags=("evenrow",))
 
-        # Style rows
-        style = ttk.Style(besucherPage)
-        style.theme_use("default")
+            # Style rows
+            style = ttk.Style(besucherPage)
+            style.theme_use("default")
 
-        # Header style
-        style.configure("Treeview.Heading", font=("Arial", 11, "bold"), background="#05445E", foreground="white")
+            # Header style
+            style.configure("Treeview.Heading", font=("Arial", 11, "bold"), background="#05445E", foreground="white")
 
-        # Row styles
-        style.configure("Treeview", font=("Arial", 11), rowheight=25)
-        table.tag_configure("evenrow", background="#D4F1F4")   # baby blue
-        # table.tag_configure("oddrow", background="white")
+            # Row styles
+            style.configure("Treeview", font=("Arial", 11), rowheight=25)
+            self.table.tag_configure("evenrow", background="#D4F1F4")   # baby blue
+            # table.tag_configure("oddrow", background="white")
 
-        # Pack table
-        table.pack(expand=True, fill="both", padx=5, pady=5)
+            # Pack table
+            self.table.pack(expand=True, fill="both", padx=5, pady=5)
 
-        # Selection highlight
-        def on_select(event):
-            selected = table.focus()
-            values = table.item(selected, "values")
-            print("Selected row:", values)
-
-        table.bind("<<TreeviewSelect>>", on_select)
-
-        return besucherPage
+            self.table.bind("<<TreeviewSelect>>", self.on_select)
+    
+    def on_select(self, event):
+        selected = self.table.focus()
+        values = self.table.item(selected, "values")
+        print("Selected row:", values)
--- login.py
+++ login.py
@@ -8,54 +8,57 @@ class LoginPage():
         self._root = root
         self._database = database
         self._style1 = style1
+        self._loginPage = ""
 
         self._besucherPage = Frame(root)
 
     def getPage(self):
-        #page
-        loginPage = Frame(self._root)
+        if not self._loginPage:
+            #page
+            loginPage = Frame(self._root)
 
-        #logo
-        logo_pil = Image.open("logo.png")
-        logo_pil = logo_pil.resize((300, 300), Image.Resampling.LANCZOS)
+            #logo
+            logo_pil = Image.open("logo.png")
+            logo_pil = logo_pil.resize((300, 300), Image.Resampling.LANCZOS)
 
-        logo_img = ImageTk.PhotoImage(logo_pil)
+            logo_img = ImageTk.PhotoImage(logo_pil)
 
-        #define columns
-        loginPage.grid_columnconfigure(0, weight=1)
-        loginPage.grid_columnconfigure(1, weight=1)
-        loginPage.grid_columnconfigure(2, weight=1)
-        loginPage.grid_columnconfigure(3, weight=1)
-        loginPage.grid_columnconfigure(4, weight=1)
+            #define columns
+            loginPage.grid_columnconfigure(0, weight=1)
+            loginPage.grid_columnconfigure(1, weight=1)
+            loginPage.grid_columnconfigure(2, weight=1)
+            loginPage.grid_columnconfigure(3, weight=1)
+            loginPage.grid_columnconfigure(4, weight=1)
 
-        #define rows
-        loginPage.grid_rowconfigure(0, weight=1)
-        loginPage.grid_rowconfigure(1, weight=1)
-        loginPage.grid_rowconfigure(2, weight=1)
-        loginPage.grid_rowconfigure(3, weight=1)
-        loginPage.grid_rowconfigure(4, weight=1)
+            #define rows
+            loginPage.grid_rowconfigure(0, weight=1)
+            loginPage.grid_rowconfigure(1, weight=1)
+            loginPage.grid_rowconfigure(2, weight=1)
+            loginPage.grid_rowconfigure(3, weight=1)
+            loginPage.grid_rowconfigure(4, weight=1)
 
-        #frames
-        form_frame = Frame(loginPage)
-        form_frame.grid(row=2, column=2, pady=20)
+            #frames
+            form_frame = Frame(loginPage)
+            form_frame.grid(row=2, column=2, pady=20)
 
-        #logo
-        img_l = Label(loginPage, image=logo_img, font=self._style1).grid(row=1, column=2)
+            #logo
+            img_l = Label(loginPage, image=logo_img, font=self._style1).grid(row=1, column=2)
 
-        #labels
-        l1 = Label(form_frame, text="Ticket:", font=self._style1).grid(row=0, column=0)
-        l2 = Label(form_frame, text="Password:", font=self._style1).grid(row=1, column=0)
+            #labels
+            l1 = Label(form_frame, text="Ticket:", font=self._style1).grid(row=0, column=0)
+            l2 = Label(form_frame, text="Password:", font=self._style1).grid(row=1, column=0)
 
-        #input fields
-        self.ticket_val= StringVar()
-        self.password_val= StringVar()
-        ip1 = Entry(form_frame, font=self._style1, bg="#D4F1F4", textvariable=self.ticket_val).grid(row=0, column=1)
-        ip2 = Entry(form_frame, font=self._style1, bg="#D4F1F4", textvariable=self.password_val, show="*").grid(row=1, column=1)
+            #input fields
+            self.ticket_val= StringVar()
+            self.password_val= StringVar()
+            ip1 = Entry(form_frame, font=self._style1, bg="#D4F1F4", textvariable=self.ticket_val).grid(row=0, column=1)
+            ip2 = Entry(form_frame, font=self._style1, bg="#D4F1F4", textvariable=self.password_val, show="*").grid(row=1, column=1)
 
-        #button
-        logBtn = Button(form_frame, text="Login", command=lambda: self.onLogin(), font=self._style1, background="#75E6DA").grid(row=2, column=0, columnspan=2)
+            #button
+            logBtn = Button(form_frame, text="Login", command=lambda: self.onLogin(), font=self._style1, background="#75E6DA").grid(row=2, column=0, columnspan=2)
 
-        return loginPage
+            self._loginPage = loginPage
+        return self._loginPage
 
     def onLogin(self):
         ticket = self.ticket_val.get()
