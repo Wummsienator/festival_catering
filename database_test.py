d10c1b4f67eaa41e02b0a9690fcafbd1406940be
Wummsienator <wummsienator@gmail.com>
2025-12-29 16:41:01 +0100

    Connect MicrosoftSqlServer

--- database_test.py
+++ database_test.py
@@ -11,19 +11,42 @@ class Database:
 
         self._cursor = cnxn.cursor()
 
-    def getproductsForStand(self, stand):
+    def getProductsForStand(self, stand):
         products = []
-        select = f"SELECT p.* FROM Products AS p INNER JOIN Stand2Product AS s2p ON s2p.ProductID = p.ProductID WHERE s2p.StandID = {stand}"
+        select = f"""
+                    SELECT p.*, s2p.Quantity FROM Products AS p 
+                    INNER JOIN Stand2Product AS s2p ON s2p.ProductID = p.ProductID 
+                    WHERE s2p.StandID = {stand}
+                 """
         for row in self._cursor.execute(select):
-            products.append({"ID": row[0], "name": row[1], "price": row[2], "time": row[3]})
+            products.append({"ID": row[0], "name": row[1], "price": row[2], "time": row[3], "quantity": row[4]})
         return products
     
     def getOrdersForTicket(self, ticket):
         orders = []
-        select = f"SELECT o.*, s.StatusText FROM Orders AS o INNER JOIN Order2Ticket AS o2t ON o2t.OrderID = o.OrderID INNER JOIN Status AS s ON s.StatusID = o.StatusID WHERE o2t.TicketNR = {ticket}"
+        select = f"""
+                    SELECT o.*, s.StatusText FROM Orders AS o 
+                    INNER JOIN Order2Ticket AS o2t ON o2t.OrderID = o.OrderID 
+                    INNER JOIN Status AS s ON s.StatusID = o.StatusID 
+                    WHERE o2t.TicketNR = {ticket}
+                 """
         for row in self._cursor.execute(select):
             orders.append({"ID": row[0], "time": row[1], "timestamp": row[2], "price": row[3], "status": row[4], "status_desc": row[7], "special_request": row[5], "stand": row[6]})
         return orders
+    
+    def getOrdersForStand(self, stand):
+        orders = []
+        select = f"""
+                    SELECT o.*, s.StatusText FROM Orders AS o 
+                    INNER JOIN Status AS s ON s.StatusID = o.StatusID 
+                    WHERE o.StandID = {stand}
+                 """
+        for row in self._cursor.execute(select):
+            orders.append({"ID": row[0], "time": row[1], "timestamp": row[2], "price": row[3], "status": row[4], "status_desc": row[7], "special_request": row[5], "stand": row[6]})
+        return orders
+    
+    def getPositionsForOrder(self, order):
+        positions = []
 
 # cursor.execute("SELECT * FROM Tickets")
 # for row in cursor:
@@ -31,5 +54,6 @@ class Database:
 
 
 test = Database()
-# print(test.getproductsForStand(1))
-print(test.getOrdersForTicket(1234567))
+# print(test.getProductsForStand(1))
+# print(test.getOrdersForTicket(1234567))
+# print(test.getOrdersForStand(3))
