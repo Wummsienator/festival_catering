e022718b32e3ac81acd53a6a2d42b60bf1218b60
hendrik.wolf <hendrik.wolf@ilcgroup.com>
2026-02-05 10:04:38 +0100

    Update stand inventory on ordering

--- database.py
+++ database.py
@@ -139,6 +139,9 @@ class Database:
                      """
             self._cursor.execute(insert)
 
+            #update stand inventory
+            self._cursor.execute(f"UPDATE Stand2Product SET Quantity = Quantity - {position_list[index]["quantity"]} WHERE ProductID = {row[0]}")
+
         #new order
         insert = """
                  INSERT INTO Orders
